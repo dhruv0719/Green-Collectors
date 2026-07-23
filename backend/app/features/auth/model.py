d4e97a61b3b473ecfa2bcf50374ff8b7ed8ebe4f
Dhruv Patil <patildhruv46@gmail.com>
2026-07-23 19:58:11 +0530

    User table added

---
+++ backend/app/features/auth/model.py
@@ -0,0 +1,16 @@
+# backend/app/features/auth/model.py
+import uuid
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+from app.config.database import Base, TimeStampMixin
+
+class User(Base, TimeStampMixin):
+    __tablename__ = "users"
+
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
+    first_name: Mapped[str] = mapped_column(String(100) , nullable=False)
+    last_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    email: Mapped[str] = mapped_column(unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(nullable=False)
+    city: Mapped[str | None] = mapped_column(nullable=True)
+    country: Mapped[str | None] = mapped_column(nullable=True)
