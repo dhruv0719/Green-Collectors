6bb5b356ff8ae77d6f2a06de929ccfcb6886cc95
Dhruv Patil <patildhruv46@gmail.com>
2026-07-23 19:58:59 +0530

    Schema of Auth system

---
+++ backend/app/features/auth/schema.py
@@ -0,0 +1,23 @@
+# backend/app/features/auth/schema.py
+from pydantic import BaseModel, EmailStr
+from uuid import UUID 
+
+class UserSignupRequest(BaseModel):
+    first_name: str
+    last_name: str
+    email: EmailStr
+    password: str
+    city: str | None = None
+    country: str | None = None
+
+class UserLoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    id: UUID
+    first_name: str
+    last_name: str
+    email: EmailStr
+    city: str | None = None
+    country: str | None = None
