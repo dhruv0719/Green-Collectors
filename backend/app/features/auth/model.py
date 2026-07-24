1e833dcbdd0d0685b80606600e5abd12bce69963
Dhruv Patil <patildhruv46@gmail.com>
2026-07-24 23:06:53 +0530

    Add UUID in id

--- backend/app/features/auth/model.py
+++ backend/app/features/auth/model.py
@@ -2,12 +2,13 @@
 import uuid
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID
 from app.config.database import Base, TimeStampMixin
 
 class User(Base, TimeStampMixin):
     __tablename__ = "users"
 
-    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True),primary_key=True, default=uuid.uuid4)
     first_name: Mapped[str] = mapped_column(String(100) , nullable=False)
     last_name: Mapped[str] = mapped_column(String(100), nullable=False)
     email: Mapped[str] = mapped_column(unique=True, nullable=False)
