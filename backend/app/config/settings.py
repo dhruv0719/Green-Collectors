8b461d125fabd92062e3a2adfc62188110b7665f
Dhruv Patil <patildhruv46@gmail.com>
2026-07-26 21:34:41 +0530

    Add JWT needful config

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -5,6 +5,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DATABASE_URL: str
 
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 30
+
     model_config = SettingsConfigDict(
         env_file=".env",
         extra="ignore"
