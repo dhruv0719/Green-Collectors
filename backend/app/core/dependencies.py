eff22bd03e16f1369c9164ac3e72ee180d7341f2
Dhruv Patil <patildhruv46@gmail.com>
2026-07-26 23:30:54 +0530

    fix url for the version1

--- backend/app/core/dependencies.py
+++ backend/app/core/dependencies.py
@@ -12,7 +12,7 @@ from app.core.security import decode_token
 from app.features.auth.repository import get_user_by_id
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
 
 async def get_current_user(db: Annotated[AsyncSession, Depends(get_db)], token: Annotated[str, Depends(oauth2_scheme)]) -> User:
     credentials_exception = HTTPException(
