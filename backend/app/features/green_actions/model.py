81ba4f7183def475cef7df84f9ef4f173125a73c
Dhruv Patil <patildhruv46@gmail.com>
2026-07-29 23:51:49 +0530

    green_action database enrty

--- backend/app/features/green_actions/model.py
+++ backend/app/features/green_actions/model.py
@@ -1,11 +1,26 @@
-# backend/app/feature/green_actions/model.py
+# backend/app/features/green_action/model.py
 import uuid
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+from sqlalchemy import String, ForeignKey, Float
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.dialects.postgresql import UUID
+
 from app.config.database import Base, TimeStampMixin
 
-class GreenAction(Base, TimeStampMixin):
-    __tablename__ = "greenaction"
+if TYPE_CHECKING:
+    from app.features.auth.model import User
 
-    pass
+
+class GreenAction(Base, TimeStampMixin):
+    __tablename__ = "green_actions"
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("users.id", ondelete="CASCADE"), nullable=False,)
+    category: Mapped[str] = mapped_column(String(100), nullable=False)
+    activity: Mapped[str] = mapped_column(String(100), nullable=False)
+    quantity: Mapped[float] = mapped_column(Float, nullable=False)
+    unit: Mapped[str] = mapped_column(String(50), nullable=False)
+    co2_saved: Mapped[float] = mapped_column(Float, nullable=False)
+    location: Mapped[str] = mapped_column(String(100), nullable=False)
+
+    user: Mapped["User"] = relationship("User", back_populates="green_actions")
