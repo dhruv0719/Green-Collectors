a4d970171774c751ca0d2d7f735375873efe3f34
Dhruv Patil <patildhruv46@gmail.com>
2026-08-01 23:07:50 +0530

    calculate co2 emitted

--- backend/app/core/carbon_calculator.py
+++ backend/app/core/carbon_calculator.py
@@ -1,9 +1,23 @@
 # backend/app/core/carbon_calculator.py
-from app.core.emission_factors import EMISSION_FACTORS
+from app.core.emission_factors import EMISSION_FACTORS, CARBON_FOOTPRINT_EMISSION_FACTORS
 from app.features.green_actions.constants import Category, Unit
+from app.features.carbon_footprint.constants import (
+    Category as CarbonCategory,
+    Unit as CarbonUnit,
+)
 
 def calculate_co2_saved(category: Category, activity: str, quantity: float, unit: Unit) -> float:
     category_data = EMISSION_FACTORS[category]
+    activity_data = category_data[activity]
+    expected_unit = activity_data["unit"]
+
+    if expected_unit != unit:
+        raise ValueError("Invalid unit for this activity")
+
+    return round(quantity * activity_data["factor"], 2)
+
+def calculate_co2_emitted(category: CarbonCategory, activity: str, quantity: float, unit: CarbonUnit) -> float:
+    category_data = CARBON_FOOTPRINT_EMISSION_FACTORS[category]
     activity_data = category_data[activity]
     expected_unit = activity_data["unit"]
 
