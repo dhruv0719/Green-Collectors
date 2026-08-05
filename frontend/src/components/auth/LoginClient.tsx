4cb7271304945f2300669c25b7cf5010ebfaf1c1
Dhruv Patil <patildhruv46@gmail.com>
2026-08-05 23:46:58 +0530

    add login client

---
+++ frontend/src/components/auth/LoginClient.tsx
@@ -0,0 +1,10 @@
+// frontend/src/components/auth/LoginClient.tsx
+"use client";
+
+import dynamic from "next/dynamic";
+
+const LoginForm = dynamic(() => import("./LoginForm"), { ssr: false });
+
+export default function LoginClient() {
+  return <LoginForm />;
+}
